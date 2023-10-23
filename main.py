f101903c2e22f2de47ba2fcdfe807da1802df3e7
Almaz Galimov <almazpython@gmail.com>
2023-10-23 17:58:45 +0500

    [upd] little updates of messages

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
 import calendar
+import random
 
 from analyzer import DataAnalyzer
 from alcocheck import AlcoAnalyzer, beer_analyzer
@@ -8,6 +9,15 @@ from settings import token, chat_id, alco_groups, ba_group, contacts
 
 
 def get_message(analyzer, period):
+    """
+    Возвращает сообщение для телеги со смайликами
+    :param analyzer:
+    :param period:
+    :return:
+    """
+    strongs = []
+    poos = {}
+
     strong = f'   \U0001F4AA\n'
     good = f'   \U0001F44D\n'
     little = f'   \U0001F90F\n'
@@ -26,19 +36,66 @@ def get_message(analyzer, period):
         message += str(tickets)
         if tickets >= 15 * multiplier:
             message += strong
+            strongs.append(result[0])
         elif tickets >= 10 * multiplier:
             message += good
         elif tickets >= 5 * multiplier:
             message += little
         else:
             message += poo
+            poos.setdefault(result[0], result[2])
+    hero = min(poos.items(), key=lambda x: x[1])
+    print(hero)
+    end_word = bad_work(hero[0])
 
-    return message
+    return message, end_word
 
 
 def alco_message(product):
     message = f'\U0001f943  У товара с кодом {product[0]} {product[1]} крепость {product[2]}. Группа {product[3]}'
     return message
+
+
+def well_done(username):
+    mess = [
+            f"Спасибо, {username}, за то, что не дает нашим компьютерам помереть молча.",
+            f"Наша поддержка похожа на чашку кофе - она всегда рядом и делает день лучше. Благодарим {username}!",
+            f"{username} - как ниндзя в мире технической поддержки. Проблемы просто исчезают, когда он в деле!",
+            f"С нашим {username} нет страха перед магическими словами '404' и 'ошибка сервера'!",
+            f"{username} не нужны красные мантии - его костюм техподдержки смотрится лучше!",
+            f"{username} наше решение для всех технических головоломок. На что бы мы делали без него?",
+            f"Кто-то позвонил красной кнопке? А нет, это просто {username}, приходящий на помощь!",
+            f"Похоже, {username} узнал секреты магии IT и щелкает пальцами, чтобы все работало!",
+            ]
+    return random.choice(mess)
+
+
+def bad_work(username):
+    mess = [
+            f"{username} решил подарить нам неплохой драматический момент, играя 'Пропал в долгах с задачами'!",
+            f"Мы слышали, что {username} начал отдыхать, потому что он закончил все задачи... в своих снах.",
+            f"Мы организуем сбор средств для {username} - каждый, кто не отставал от плана, может скинуться на билет в будущее.",
+            f"Считаем, что {username} взял на себя роль 'Двигатель экономии времени'. Мы ждем его производительных результатов!",
+            f"Думаю, {username} решил устроить тайный мастер-класс 'Как увеличить количество невыполненных задач'.",
+            f"Иногда {username} становится таким невидимым, что мы подозреваем, что он исследует науку камуфляжа.",
+            f"Новое хобби {username}: не заниматься производительностью... ",
+            f"Пока все мы учились решать задачи, {username} мастерил план 'Исчезновения из списка задач'.",
+            f"Секрет успеха {username}: делать все медленнее, чем кажется возможным. Так, никто и не заметит!",
+            f"Если {username} был бы профессиональным спящим, он был бы богат. Научитесь многому, наблюдая за ним!",
+            f"Загадка {username}: почему он всегда такой медленный? Он, возможно, пытается победить в 'Медленном марафоне задач'!",
+            f"Важно помнить, что {username} обратил задачу в искусство: искусство не заканчивать ничего.",
+            f"Если {username} учился бы на факультете 'Отсрочки и оправдания', он был бы лучшим!",
+            f"Мы подозреваем, что {username} работает по плану: 'Завалить всё, чтобы потом чудесно спасти'.",
+            f"Секрет {username}: он считает, что лучшая задача - это задача, которую не нужно выполнять.",
+            f"Кажется, {username} принял решение пойти на 'Отдыхательскую Олимпиаду' и выиграть золотую медаль в лежании на диване!",
+            f"Пока все трудились, {username} исследовал феномен 'Исчезновение с рабочего места'.",
+            f"Самый сложный вызов: попробовать понять, как {username} считает, что это нормально.",
+            f"Если {username} учился бы в Школе Тормозологии, он был бы профессионалом!",
+            f"Серьезное исследование: как {username} умудряется сделать меньше, чем ничего.",
+            f"Мы думали, {username} - глава 'Клуба последнего минутчика'. Оказалось, он - единственный член!",
+            f"Подсказка для {username}: чтобы сделать что-то, нужно начать сделывать что-то. Попробуйте!",
+        ]
+    return random.choice(mess)
 
 
 def main():
@@ -50,9 +107,10 @@ def main():
 
     telegram_api = TelegramBot(token=token, chat_id=chat_id)
 
-    message = get_message(analyzer, 'сегодня')
+    message, finish = get_message(analyzer, 'сегодня')
 
     telegram_api.send_message(message)
+    telegram_api.send_message(finish)
 
     message = f'Открытых заявок осталось: {analyzer.total_open[0][0]}\n\n'
     message += f'Самая старая заявка {analyzer.total_open[0][1]}'
@@ -61,19 +119,21 @@ def main():
 
     # если сегодня воскресенье
     if datetime.datetime.today().weekday() == 6:
-        message = get_message(analyzer, 'месяц')
+        message, finish = get_message(analyzer, 'месяц')
 
-        print(message)
         telegram_api.send_message(message)
+        print(message)
+        telegram_api.send_message(finish)
 
     # если сегодня последний день месяца
     today = datetime.date.today()
     is_last_day_of_month = today.day == calendar.monthrange(today.year, today.month)[1]
     if is_last_day_of_month:
-        message = get_message(analyzer, 'месяц')
+        message, finish = get_message(analyzer, 'месяц')
 
-        print(message)
         telegram_api.send_message(message)
+        print(message)
+        telegram_api.send_message(finish)
 
     # дальше идет проверка алкоголя
     for k, v in contacts.items():
@@ -95,6 +155,7 @@ def main():
                 print(alco_message(beer))
         except Exception as err:
             print(err)
+
 
 if __name__ == "__main__":
     main()
